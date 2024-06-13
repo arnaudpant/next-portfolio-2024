a81cc7b53935c2d6722689515c421acd7b6a3ec1
macbook kal <arnaud.pantenier@gmail.com>
2024-06-13 18:46:08 +0200

    FIRST COMMIT

---
+++ components/Nav.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links: NavLinks[] = [
+    {
+        name: "accueil",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "a propos",
+        path: "/about",
+    },
+    {
+        name: "portfolio",
+        path: "/portfolio",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    },
+];
+
+const Nav = () => {
+    const pathname = usePathname();
+    return (
+        <nav className="flex gap-8">
+            {links.map((link, index) => (
+                <Link
+                    key={index}
+                    href={link.path}
+                    className={`${
+                        link.path === pathname &&
+                        "text-accent border-b-2 border-accent"
+                    } capitalize font-medium hover:text-accent transition-all`}
+                >
+                    {link.name}
+                </Link>
+            ))}
+        </nav>
+    );
+};
+
+export default Nav;
---
+++ components/PageTransition.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const PageTransition = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    const pathname = usePathname();
+    return (
+        <AnimatePresence>
+            <motion.div
+                initial={{ opacity: 1 }}
+                animate={{
+                    opacity: 0,
+                    transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+                }}
+                className="h-screen w-screen fixed bg-zinc-900 top-0 pointer-events-none"
+            />
+            <div key={pathname}>{children}</div>
+        </AnimatePresence>
+    );
+};
+
+export default PageTransition;
